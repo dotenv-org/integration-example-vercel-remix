4afe284c6a0b1778d493e731d44ed9d0a263dd47
Scott Motte <mot@mot.la>
2022-10-17 09:54:17 -0700

    Add ENV

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,18 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  return json({
+    ENV: {
+      HELLO: process.env.HELLO,
+    },
+  });
+}
+
 export default function Index() {
+  const data = useLoaderData()
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Hello World</h1>
-    </div>
+    <h1>Hello {data.ENV.HELLO}</h1>
   );
 }
